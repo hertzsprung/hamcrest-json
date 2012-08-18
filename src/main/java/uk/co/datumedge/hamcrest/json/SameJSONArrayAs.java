4fce7ff439e2482fa4caa4d4ab95df0abd4d3bbe
James Shaw <js102@zepler.net>
2012-08-18 20:32:47 +0100

    Add negative test for SameJSONArrayAs

--- src/main/java/uk/co/datumedge/hamcrest/json/SameJSONArrayAs.java
+++ src/main/java/uk/co/datumedge/hamcrest/json/SameJSONArrayAs.java
@@ -1,18 +1,25 @@
 package uk.co.datumedge.hamcrest.json;
 
+import static org.skyscreamer.jsonassert.JSONCompare.compareJSON;
+import static org.skyscreamer.jsonassert.JSONCompareMode.STRICT_ORDER;
+
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONCompareResult;
 
 /**
  * Matcher that asserts that one JSONArray is the same as another.
  */
 public class SameJSONArrayAs extends TypeSafeDiagnosingMatcher<JSONArray> {
 
+	private JSONArray expected;
+
 	public SameJSONArrayAs(JSONArray expected) {
-		// TODO Auto-generated constructor stub
+		this.expected = expected;
 	}
 
 	@Override
@@ -22,9 +29,13 @@ public class SameJSONArrayAs extends TypeSafeDiagnosingMatcher<JSONArray> {
 	}
 
 	@Override
-	protected boolean matchesSafely(JSONArray item, Description mismatchDescription) {
-		// TODO Auto-generated method stub
-		return true;
+	protected boolean matchesSafely(JSONArray actual, Description mismatchDescription) {
+		try {
+			JSONCompareResult result = compareJSON(expected, actual, STRICT_ORDER);
+			return result.passed();
+		} catch (JSONException e) {
+			return true;
+		}
 	}
 	
 	@Factory
--- src/test/java/uk/co/datumedge/hamcrest/json/SameJSONArrayAsTest.java
+++ src/test/java/uk/co/datumedge/hamcrest/json/SameJSONArrayAsTest.java
@@ -1,13 +1,19 @@
 package uk.co.datumedge.hamcrest.json;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
 import static uk.co.datumedge.hamcrest.json.SameJSONArrayAs.sameJSONArrayAs;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.Test;
 
 public class SameJSONArrayAsTest {
 	@Test public void matchesEmptyJSONArrays() {
 		assertThat(new JSONArray(), sameJSONArrayAs(new JSONArray()));
+	}
+	
+	@Test public void doesNotMatchOneEmptyAndOneNonEmptyJSONArray() throws JSONException {
+		assertThat(new JSONArray(), not(sameJSONArrayAs(new JSONArray("[263]"))));
 	}
 }
