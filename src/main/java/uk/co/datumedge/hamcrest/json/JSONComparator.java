a0a6ee994a6f4f0d3b75f2447fa964d3f02d52e4
James Shaw <js102@zepler.net>
2012-08-21 22:45:42 +0100

    little more javadoc

--- src/main/java/uk/co/datumedge/hamcrest/json/JSONComparator.java
+++ src/main/java/uk/co/datumedge/hamcrest/json/JSONComparator.java
@@ -4,6 +4,8 @@ import org.json.JSONException;
 
 /**
  * Compares JSON documents.
+ * 
+ * @param <T> the document type, typically {@code JSONObject} or {@code JSONArray}.
  */
 public interface JSONComparator<T> {
 	JSONComparisonResult compare(T expected, T actual) throws JSONException;
