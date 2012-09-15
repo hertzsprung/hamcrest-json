9c44fdbcc46ca8ec577120b490176fb562e587eb
James Shaw <js102@zepler.net>
2012-09-15 21:27:30 +0100

    List missing fields in diagnostics

--- src/main/java/uk/co/datumedge/hamcrest/json/JSONAssertComparisonResult.java
+++ src/main/java/uk/co/datumedge/hamcrest/json/JSONAssertComparisonResult.java
@@ -1,9 +1,6 @@
 package uk.co.datumedge.hamcrest.json;
 
-import static uk.co.datumedge.hamcrest.json.JSONComparisonResult.comparisonFailed;
 import static uk.co.datumedge.hamcrest.json.JSONComparisonResult.comparisonPassed;
-
-import java.util.List;
 
 import org.hamcrest.Description;
 import org.hamcrest.SelfDescribing;
@@ -14,27 +11,36 @@ final class JSONAssertComparisonResult {
 	private JSONAssertComparisonResult() { }
 
 	static JSONComparisonResult resultOf(JSONCompareResult result) {
-		if (result.isFailureOnField()) {
-			return diagnose(result.getFieldFailures());
-		} else if (result.failed()) {
-			return comparisonFailed(result.getMessage());
+		if (result.failed()) {
+			return diagnose(result);
 		} else {
 			return comparisonPassed();
 		}
 	}
 
-	private static JSONComparisonResult diagnose(final List<FieldComparisonFailure> fieldFailures) {
+	private static JSONComparisonResult diagnose(final JSONCompareResult result) {
 		return new JSONComparisonResult(new SelfDescribing() {
 			@Override
 			public void describeTo(Description description) {
 				boolean first = true;
-				for (FieldComparisonFailure failure : fieldFailures) {
+
+				for (String field : result.getMissingFields()) {
+					if (!first) description.appendText(" and ");
+					description.appendText("missing field ").appendText(field);
+					first = false;
+				}
+
+				for (FieldComparisonFailure failure : result.getFieldFailures()) {
 					if (!first) description.appendText(" and ");
 					description
 						.appendText("field ").appendText(failure.getField())
 						.appendText(" was ").appendValue(failure.getActual())
 						.appendText(" instead of ").appendValue(failure.getExpected());
 					first = false;
+				}
+
+				if (result.getFieldFailures().isEmpty() && result.getMissingFields().isEmpty()) {
+					description.appendText(result.getMessage());
 				}
 			}
 		});
--- src/test/java/uk/co/datumedge/hamcrest/json/SameJSONAsTest.java
+++ src/test/java/uk/co/datumedge/hamcrest/json/SameJSONAsTest.java
@@ -27,43 +27,54 @@ public class SameJSONAsTest {
 	private static final String EXCEPTION_MESSAGE = "exception message";
 	private final JSONArray actual = new JSONArray("[13, 85]");
 	private final JSONArray expected = new JSONArray("[42, 63]");
-	
+
 	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
 	@SuppressWarnings("unchecked") private final JSONModalComparator<JSONArray> jsonComparator = context.mock(JSONModalComparator.class);
 	
 	public SameJSONAsTest() throws JSONException {
 	}
-	
+
 	@Test public void matchesEmptyJSONArrays() {
 		assertThat(new JSONArray(), is(sameJSONArrayAs(new JSONArray())));
 	}
-	
+
 	@Test public void doesNotMatchOneEmptyAndOneNonEmptyJSONArray() throws JSONException {
 		assertThat(new JSONArray(), is(not(sameJSONArrayAs(new JSONArray("[263]")))));
 	}
-	
+
 	@Test public void appendsExpectedStringValueToDescription() throws JSONException {
 		StringDescription description = new StringDescription();
 		sameJSONArrayAs(new JSONArray("[831]")).describeTo(description);
 		assertThat(description.toString(), is("\"[831]\""));
 	}
-	
-	@Test public void appendsTextualComparisonToMismatchDescription() throws JSONException {
-		StringDescription mismatchDescription = new StringDescription();
+
+	@Test public void appendsFieldMismatchToMismatchDescription() throws JSONException {
 		SameJSONAs<JSONArray> matcher = new SameJSONAs<JSONArray>(expected, modalComparatorFor(jsonArrayComparison()));
-		matcher.matches(actual);
-		matcher.describeMismatch(actual, mismatchDescription);
-		assertThat(mismatchDescription, both(
-				containsString("13")).and(containsString("42"))
-				.and(containsString("85")).and(containsString("63")));
+		assertThat(mismatchDescriptionFor(actual, matcher), both(
+				containsString("13")).and(containsString("42")).and(containsString("85")).and(containsString("63")));
+	}
+
+	@Test public void appendsMissingKeyToMismatchDescription() throws JSONException {
+		assertThat(mismatchDescriptionFor("{}", sameJSONAs("{\"missingKey\": 6}")), containsString("missingKey"));
 	}
 	
+	@Test public void appendsFailureStringToMismatchDescription() throws JSONException {
+		assertThat(mismatchDescriptionFor("{\"myField\":[3]}", sameJSONAs("{\"myField\": 5}")), is(not("")));
+	}
+
+	private <T> StringDescription mismatchDescriptionFor(T actual, SameJSONAs<? super T> matcher) {
+		StringDescription mismatchDescription = new StringDescription();
+		matcher.matches(actual);
+		matcher.describeMismatch(actual, mismatchDescription);
+		return mismatchDescription;
+	}
+
 	@Test public void doesNotMatchWhenJSONExceptionIsCaught() throws JSONException {
 		allowingJSONComparatorToThrowJSONException();
 		
 		assertThat(actual, is(not(sameJSONArrayAs(expected, jsonComparator))));
 	}
-	
+
 	@Test public void appendsJSONExceptionStacktraceToMismatchDescription() throws JSONException {
 		allowingJSONComparatorToThrowJSONException();
 		
@@ -73,25 +84,25 @@ public class SameJSONAsTest {
 		matcher.describeMismatch(actual, mismatchDescription);
 		assertThat(mismatchDescription, both(containsString(EXCEPTION_MESSAGE)).and(containsString(SameJSONAs.class.getName())));
 	}
-	
+
 	@Test public void doesNotMatchWhenActualJSONHasExtraFields() throws JSONException {
 		assertThat(new JSONArray("[{'a':3, 'b':5}, 2]"), is(not(sameJSONArrayAs(new JSONArray("[{'a':3}, 2]")))));
 	}
-	
+
 	@Test public void matchesWithAdditionalFieldsInActualJSONArray() throws JSONException {
 		assertThat(new JSONArray("[{\"foo\":6, \"bar\":1}, 2]"), sameJSONArrayAs(new JSONArray("[{\"bar\":1}, 2]")).allowingExtraUnexpectedFields());
 	}
-	
+
 	@Test public void matchesJSONArrayHavingElementsInAnyOrder() throws JSONException {
 		assertThat(new JSONArray("[1, 5, 2]"), sameJSONArrayAs(new JSONArray("[5, 2, 1]")).allowingAnyArrayOrdering());
 	}
-	
+
 	@Test public void doesNotMatchJSONArrayHavingElementsInAnyOrderWithAdditionalFieldsInExpectedJSONObject() throws JSONException {
 		assertThat(
 				new JSONArray("[{\"b\":3, \"arr\":[1, 5, 2]}]"),
 				not(sameJSONArrayAs(new JSONArray("[{\"arr\":[5, 2, 1]}]")).allowingAnyArrayOrdering()));
 	}
-	
+
 	@Test public void matchesJSONArrayWithAdditionalFieldsInExpectedJSONArrayHavingElementsInAnyOrder() throws JSONException {
 		assertThat(
 				new JSONArray("[{\"b\":3, \"arr\":[1, 5, 2]}]"),
@@ -99,7 +110,7 @@ public class SameJSONAsTest {
 					.allowingAnyArrayOrdering()
 					.allowingExtraUnexpectedFields());
 	}
-	
+
 	@Test public void matchesEmptyJSONObjects() {
 		assertThat(new JSONObject(), is(sameJSONObjectAs(new JSONObject(), modalComparatorFor(jsonObjectComparison()))));
 	}
@@ -107,27 +118,27 @@ public class SameJSONAsTest {
 	@Test public void doesNotMatchOneEmptyAndOneNonEmptyJSONObject() throws JSONException {
 		assertThat(new JSONObject(), is(not(sameJSONObjectAs(new JSONObject("{\"foo\":3}")))));
 	}
-	
+
 	@Test public void matchesWithAdditionalFieldsInActualJSONObject() throws JSONException {
 		assertThat(new JSONObject("{\"a\":3,\"b\":7}"), sameJSONObjectAs(new JSONObject("{\"b\":7}")).allowingExtraUnexpectedFields());
 	}
-	
+
 	@Test public void doesNotMatchWithAdditionalFieldsInExpectedJSONObject() throws JSONException {
 		assertThat(new JSONObject("{\"b\":7}"), not(sameJSONObjectAs(new JSONObject("{\"a\":3,\"b\":7}")).allowingExtraUnexpectedFields()));
 	}
-	
+
 	@Test public void matchesJSONObjectHavingArrayElementsInAnyOrder() throws JSONException {
 		assertThat(
 				new JSONObject("{\"arr\":[1, 5, 2]}"),
 				sameJSONObjectAs(new JSONObject("{\"arr\":[5, 2, 1]}")).allowingAnyArrayOrdering());
 	}
-	
+
 	@Test public void doesNotMatchJSONObjectHavingArrayElementsInAnyOrderWithAdditionalFieldsInExpectedJSONObject() throws JSONException {
 		assertThat(
 				new JSONObject("{\"b\":3, \"arr\":[1, 5, 2]}"),
 				not(sameJSONObjectAs(new JSONObject("{\"arr\":[5, 2, 1]}")).allowingAnyArrayOrdering()));
 	}
-	
+
 	@Test public void matchesJSONObjectWithAdditionalFieldsInExpectedJSONObjectAndHavingArrayElementsInAnyOrder() throws JSONException {
 		assertThat(
 				new JSONObject("{\"b\":3, \"arr\":[1, 5, 2]}"),
@@ -135,7 +146,7 @@ public class SameJSONAsTest {
 					.allowingExtraUnexpectedFields()
 					.allowingAnyArrayOrdering());
 	}
-	
+
 	@Test public void matchesJSONObjectsAsStrings() {
 		assertThat("{\"foo\": 5}", sameJSONAs("{\"foo\": 5}", modalComparatorFor(stringComparison())));
 	}
